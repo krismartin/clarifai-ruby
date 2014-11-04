cad6a2a8803b0ee0dd8c055cecdf37ee371d903d
Kris Martin <kris.sugandhi@gmail.com>
2014-11-04 16:02:05 +1100

    More granular 401 errors

--- lib/clarifai/error.rb
+++ lib/clarifai/error.rb
@@ -8,6 +8,15 @@ module Clarifai
   # Raised when Clarifai returns the HTTP status code 401
   class Unauthorized < Error; end
 
+  # Raised when Clarifai returns the HTTP status code 401
+  class ExpiredToken < Unauthorized; end
+
+  # Raised when Clarifai returns the HTTP status code 401
+  class InvalidToken < Unauthorized; end
+
+  # Raised when Clarifai returns the HTTP status code 401
+  class TokenRequired < Unauthorized; end
+
   # Raised when Clarifai returns the HTTP status code 500
   class InternalServerError < Error; end
 end
--- lib/faraday/raise_http_exceptions.rb
+++ lib/faraday/raise_http_exceptions.rb
@@ -6,11 +6,22 @@ module FaradayMiddleware
   class RaiseHttpException < Faraday::Middleware
     def call(env)
       @app.call(env).on_complete do |response|
-        case response[:status].to_i
+        response_status = response[:status].to_i
+        status_code, status_msg = status_code_and_message(response[:body])
+        case response_status
         when 400
-          raise Clarifai::BadRequest, error_message_400(response)
+          raise Clarifai::BadRequest, error_message_400(response, status_code, status_msg)
         when 401
-          raise Clarifai::Unauthorized, error_message_400(response)
+          case status_code
+          when 'TOKEN_NONE'
+            raise Clarifai::TokenRequired, error_message_400(response, status_code, status_msg)
+          when 'TOKEN_APP_INVALID', 'TOKEN_INVALID'
+            raise Clarifai::InvalidToken, error_message_400(response, status_code, status_msg)
+          when 'TOKEN_EXPIRED'
+            raise Clarifai::ExpiredToken, error_message_400(response, status_code, status_msg)
+          else
+            raise Clarifai::Unauthorized, error_message_400(response, status_code, status_msg)
+          end
         when 500
           raise Clarifai::InternalServerError, error_message_500(response, "Something is technically wrong.")
         end
@@ -24,22 +35,33 @@ module FaradayMiddleware
 
     private
 
-    def error_message_400(response)
-      "#{response[:method].to_s.upcase} #{response[:url].to_s}: #{response[:status]}#{error_body(response[:body])}"
+    def error_message_400(response, status_code, status_msg)
+      "#{response[:method].to_s.upcase} #{response[:url].to_s}: #{response[:status]}#{error_body(status_code, status_msg)}"
     end
 
-    def error_body(body)
+    def body_json(body)
       # body gets passed as a string, not sure if it is passed as something else from other spots?
       if not body.nil? and not body.empty? and body.kind_of?(String)
         # removed multi_json thanks to wesnolte's commit
-        body = ::JSON.parse(body)
+        return ::JSON.parse(body)
       end
+    end
+
+    def status_code_and_message(body)
+      body = body_json(body)
 
       if body.nil?
-        nil
+        return nil, nil
       elsif body['status_msg'] and not body['status_msg'].empty?
-        ": #{body['status_code']}: #{body['status_msg']}"
+        return body['status_code'], body['status_msg']
       end
+    end
+
+    def error_body(status_code, status_msg)
+      error_body = ""
+      error_body << ": #{status_code}" if status_code.present?
+      error_body << ": #{status_msg}" if status_msg.present?
+      return error_body
     end
 
     def error_message_500(response, body=nil)
