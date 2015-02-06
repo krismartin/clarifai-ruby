da7ab722f2f089ff1e286ecc21fb708f5ee2c0dd
Kris Martin <kris.sugandhi@gmail.com>
2015-02-06 15:05:19 +1100

    Changing arguments in 'search' and 'similar_search' methods

--- lib/clarifai/client/curator_search.rb
+++ lib/clarifai/client/curator_search.rb
@@ -2,22 +2,22 @@ module Clarifai
   class Client
     # Defines methods related to Curator Index management
     module CuratorSearch
-      def search(index, query, size=50, page=0, search_options={})
+      def search(index, query, size=50, start=0, search_options={})
         options = {
           query: { query_string: query },
           num: size,
-          start: page
+          start: start
         }
         response = post("curator/#{index}/search", options.to_json, params_encoder, encode_json=true)
         response
       end
 
-      def similar_search(index, image_urls, size=50, page=0, search_options={})
+      def similar_search(index, image_urls, size=50, start=0, search_options={})
         image_urls = [image_urls] if image_urls.is_a? String
         options = {
           query: { similar_urls: image_urls },
           num: size,
-          start: page
+          start: start
         }
         response = post("curator/#{index}/search", options.to_json, params_encoder, encode_json=true)
         response
