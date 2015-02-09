39f620f14b45a119396540c999fdb24059cd65a3
Kris Martin <kris.sugandhi@gmail.com>
2015-02-10 10:56:41 +1100

    Add support for searching using tags operator

--- lib/clarifai/client/curator_search.rb
+++ lib/clarifai/client/curator_search.rb
@@ -2,14 +2,28 @@ module Clarifai
   class Client
     # Defines methods related to Curator Index management
     module CuratorSearch
-      def search(index, query, size=50, start=0, search_options={})
+      def search(index, search_options={})
+        response = post("curator/#{index}/search", search_options.to_json, params_encoder, encode_json=true)
+        response
+      end
+
+      def query_string_search(index, query, size=50, start=0, search_options={})
         options = {
           query: { query_string: query },
           num: size,
           start: start
         }
-        response = post("curator/#{index}/search", options.to_json, params_encoder, encode_json=true)
-        response
+        search(index, options)
+      end
+
+      def tags_search(index, tags, size=50, start=0, search_options={})
+        tags = [tags] if tags.is_a? String
+        options = {
+          query: { tags: tags },
+          num: size,
+          start: start
+        }
+        search(index, options)
       end
 
       def similar_search(index, image_urls, size=50, start=0, search_options={})
@@ -19,8 +33,7 @@ module Clarifai
           num: size,
           start: start
         }
-        response = post("curator/#{index}/search", options.to_json, params_encoder, encode_json=true)
-        response
+        search(index, options)
       end
     end
   end
