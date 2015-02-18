2496fe9e54a0ac36260a97aa0a163aeba4012b79
Kris Martin <kris.sugandhi@gmail.com>
2015-02-18 15:02:17 +1100

    Add support for filtering search by specific metadata field

--- lib/clarifai/client/curator_search.rb
+++ lib/clarifai/client/curator_search.rb
@@ -7,16 +7,29 @@ module Clarifai
         response
       end
 
-      def query_string_search(index, query, size=50, start=0, search_options={})
+      def query_string_search(index, query, size=50, start=0, metadata_filters={})
         options = {
-          query: { query_string: query },
           num: size,
           start: start
         }
+
+        if metadata_filters.present?
+          options[:query] = {
+            bool: {
+              must: [
+                { query_string: query },
+                { term: metadata_filters }
+              ]
+            }
+          }
+        else
+          options[:query] = { query_string: query }
+        end
+
         search(index, options)
       end
 
-      def tags_search(index, tags, size=50, start=0, search_options={})
+      def tags_search(index, tags, size=50, start=0, metadata_filters={})
         tags = [tags] if tags.is_a? String
 
         options = {
@@ -24,23 +37,54 @@ module Clarifai
           start: start
         }
 
-        if tags.is_a? Array
-          options[:query] = { tags: tags }
-        elsif tags.is_a? Hash
-          options[:query] = { tags: { tags: tags } }
+        if metadata_filters.present?
+          options[:query] = {
+            bool: {
+              must: [
+                construct_query_tags(tags),
+                { term: metadata_filters }
+              ]
+            }
+          }
+        else
+          options[:query] = construct_query_tags(tags)
         end
 
         search(index, options)
       end
 
-      def similar_search(index, image_urls, size=50, start=0, search_options={})
+      def similar_search(index, image_urls, size=50, start=0, metadata_filters={})
         image_urls = [image_urls] if image_urls.is_a? String
         options = {
-          query: { similar_urls: image_urls },
           num: size,
           start: start
         }
+
+        if metadata_filters.present?
+          options[:query] = {
+            bool: {
+              must: [
+                { similar_urls: image_urls },
+                { term: metadata_filters }
+              ]
+            }
+          }
+        else
+          options[:query] = { similar_urls: image_urls }
+        end
+
         search(index, options)
+      end
+
+      private
+      def construct_query_tags(tags)
+        query = {}
+        if tags.is_a? Array
+          query[:tags] = tags
+        elsif tags.is_a? Hash
+          query[:tags] = { tags: tags }
+        end
+        return query
       end
     end
   end
