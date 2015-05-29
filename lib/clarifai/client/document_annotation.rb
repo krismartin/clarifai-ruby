9ab7801a5f27183ce55c060ae8412b0f338099eb
Kris Martin <kris.sugandhi@gmail.com>
2015-05-29 16:09:35 +1000

    Added service for updating document annotations

--- lib/clarifai/client/document_annotation.rb
+++ lib/clarifai/client/document_annotation.rb
@@ -2,6 +2,19 @@ module Clarifai
   class Client
     # Defines methods related to Document Annotation management
     module DocumentAnnotation
+      def update_document_annotation(collection_id, doc_id, actioner, namespace, annotations, score=1.0)
+        params = { annotation_set: { namespace: namespace, annotations: [] } }
+        annotations.each do |annotation|
+          params[:annotation_set][:annotations] << {
+            tag: { cname: annotation },
+            score: score
+          }
+        end
+
+        response = put("curator/collections/#{collection_id}/documents/#{doc_id}/annotations", params.to_json, params_encoder, encode_json=true)
+        response
+      end
+
       def delete_document_annotation(collection_id, doc_id, actioner, namespace, annotations)
         params = { annotation_set: { namespace: namespace, annotations: [] } }
         annotations.each do |annotation|
