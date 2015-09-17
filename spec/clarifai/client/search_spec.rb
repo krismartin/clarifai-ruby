e6c95dca75bc5384d2ea9d275b409690e8b84032
Kris Martin <kris.sugandhi@gmail.com>
2015-09-17 17:57:04 +1000

    Search spec (WIP)

---
+++ spec/clarifai/client/search_spec.rb
@@ -0,0 +1,98 @@
+require 'spec_helper'
+
+class Clarifai::Client::SearchSpec < MiniTest::Spec
+  @@client = nil
+  @@search_response = nil
+
+  let(:collection_name) { collection_id + "-search_test" }
+
+  before do
+    if @@client.nil? || @@search_response.nil?
+      Clarifai.reset
+      @@client = Clarifai::Client.new(client_id: client_id, client_secret: client_secret, collection_id: collection_name)
+      create_collection(@@client, collection_name)
+      create_documents(@@client, collection_name)
+      @@search_response = @@client.search collection_name, tags: ["nobody"]
+    end
+  end
+
+  describe Clarifai::Client do
+
+    describe ".search" do
+
+      describe "search response" do
+        it "should have status" do
+          @@search_response.status.status.must_equal "OK"
+        end
+
+        it "should have total number of results" do
+          @@search_response.total_num_results.must_be_kind_of Integer
+        end
+
+        it "should have an array results" do
+          @@search_response.results.must_be_kind_of Array
+        end
+      end
+
+      describe "search results" do
+        it "should be ordered by overall scores" do
+          scores = @@search_response.results.collect{|r| r.score}
+          @@search_response.results.collect{|r| r.score}.must_equal scores.sort.reverse
+        end
+      end
+
+      describe "search result item" do
+        it "should have overall score" do
+          search_item = @@search_response.results.first
+          search_item.score.must_be_kind_of Float
+        end
+
+        it "should have document" do
+          search_item = @@search_response.results.first
+          search_item.document.wont_be_nil
+        end
+
+        describe "document" do
+          it "should have annotation_sets" do
+            document = @@search_response.results.first.document
+            document.annotation_sets.must_be_kind_of Array
+          end
+
+          it "should have media_refs" do
+            document = @@search_response.results.first.document
+            document.media_refs.must_be_kind_of Array
+          end
+
+          it "should have docid" do
+            document = @@search_response.results.first.document
+            document.docid.wont_be_nil
+          end
+
+          it "should have metadata" do
+            document = @@search_response.results.first.document
+            document.metadata.must_be_kind_of Hash
+          end
+        end
+      end
+
+      describe "search with pagination" do
+        it "should paginate the results" do
+          unpaginated_results = @@search_response.results.collect{|r| r.document.docid}
+          total_num_results = @@search_response.total_num_results
+          per_page = 2
+          total_pages = (total_num_results + per_page - 1) / per_page
+
+          total_pages.times do |index|
+            page = index+1
+            start = (page==1 ? 0 : ((page-1)*per_page))
+            response = @@client.search collection_name, tags: ['nobody'], per_page: per_page, start: start
+            results = response.results.collect{|r| r.document.docid}
+            results.must_equal unpaginated_results[start..(start+per_page)-1]
+          end
+        end
+      end
+
+    end
+
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,6 +24,23 @@ def create_document(client, collection, doc)
   return client.create_document collection, doc[:id], doc[:url], doc[:metadata]
 end
 
+def create_documents(client, collection)
+  [
+    { id: "image_1", url: "http://farm3.staticflickr.com/2063/5742404359_fce5e850bd_b.jpg", type: "image/jpeg", metadata: { "title" => "Ocean", "photographer_name" => "Evans Dare", "photographer_id" => "photographer_1", "orientation" => "landscape", "license_type" => "royalty-free" } },
+    { id: "image_2", url: "http://farm7.staticflickr.com/6021/6004280141_bd421425af_b.jpg", type: "image/jpeg", metadata: { "title" => "Crowd at soccer stadium", "photographer_name" => "Pablo Champlin", "photographer_id" => "photographer_2", "orientation" => "landscape", "license_type" => "royalty-free" } },
+    { id: "image_3", url: "http://farm4.staticflickr.com/3720/18644414989_e1c248e0b1_b.jpg", type: "image/jpeg", metadata: { "title" => "NYC Skyline", "photographer_name" => "Israel Bode", "photographer_id" => "photographer_3", "orientation" => "landscape", "license_type" => "rights-managed" } },
+    { id: "image_4", url: "http://farm8.staticflickr.com/7391/10398896814_ce60a772c4_b.jpg", type: "image/jpeg", metadata: { "title" => "Yellow Cab, NYC", "photographer_name" => "Israel Bode", "photographer_id" => "photographer_3", "orientation" => "landscape", "license_type" => "royalty-free" } },
+    { id: "image_5", url: "http://farm6.staticflickr.com/5068/5689835339_33ac1745ca_b.jpg", type: "image/jpeg", metadata: { "title" => "Telephone Booth", "photographer_name" => "Emmett Kuhlman", "photographer_id" => "photographer_4", "orientation" => "portrait", "license_type" => "royalty-free" } },
+    { id: "image_6", url: "http://farm6.staticflickr.com/5323/18223890844_baa427cbbe_b.jpg", type: "image/jpeg", metadata: { "title" => "Sunset, Waikiki Beach", "photographer_name" => "Evelyn Lehner", "photographer_id" => "photographer_5", "orientation" => "landscape", "license_type" => "rights-managed" } },
+    { id: "image_7", url: "http://farm5.staticflickr.com/4110/5194747168_daa9bd6f47_b.jpg", type: "image/jpeg", metadata: { "title" => "Taxi in Japan", "photographer_name" => "Robert Wiza", "photographer_id" => "photographer_6", "orientation" => "landscape", "license_type" => "rights-managed" } },
+    { id: "image_8", url: "http://farm3.staticflickr.com/2788/4424287937_7fa119b45d_b.jpg", type: "image/jpeg", metadata: { "title" => "Great Ocean Road", "photographer_name" => "Jane Williamson", "photographer_id" => "photographer_7", "orientation" => "landscape", "license_type" => "royalty-free" } },
+    { id: "image_9", url: "http://farm3.staticflickr.com/2719/4300373354_a5c867e3b3_b.jpg", type: "image/jpeg", metadata: { "title" => "Puppy Pug", "photographer_name" => "Sam Smith", "photographer_id" => "photographer_8", "orientation" => "landscape", "license_type" => "royalty-free" } },
+    { id: "image_10", url: "http://farm4.staticflickr.com/3343/3558798075_5542d0b1fc_b.jpg", type: "image/jpeg", metadata: { "title" => "Elephant", "photographer_name" => "Sam Smith", "photographer_id" => "photographer_8", "orientation" => "landscape", "license_type" => "royalty-free" } }
+  ].each do |doc|
+    create_document(client, collection, doc)
+  end
+end
+
 def get_document(client, collection, doc_id)
   return client.get_document collection, doc_id
 end
@@ -34,7 +51,7 @@ end
 
 Minitest.after_run {
   client = Clarifai::Client.new(client_id: client_id, client_secret: client_secret)
-  ["collection_test", "collection_test-2", "document_test", "document_metadata_test", "document_annotation_test"].each do |postfix|
+  ["collection_test", "collection_test-2", "document_test", "document_metadata_test", "document_annotation_test", "search_test"].each do |postfix|
     client.delete_collection "#{collection_id}-#{postfix}"
   end
 }
